eb0754247ce8108a140631e255b6ecc15e650457
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-08-25 11:47:42 -0500

    Added TextureLoader and created a game board
    Added a THREE.TextureLoader() to load custom textures to the game. The primitive object "Cube" has been replaced with a game board setup, including a textured plane and a grid helper for better visualization. The view of the camera has been adjusted to fit the new board. An empty game board array was added for future game logic implementation.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+const loader = new THREE.TextureLoader();
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,15 +8,41 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// Loading and applying the texture
+loader.load('public/pandav-tank-88-KbSZbuEc-unsplash.jpg', function ( texture ) {
+    const material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.DoubleSide } );
 
-camera.position.z = 5;
+    const boardSize = 10;
+    let halfSize = boardSize / 2;
+    let boardPlaneGeometry = new THREE.PlaneGeometry(boardSize, boardSize);
+
+    // Add the game board as a simple plane first.
+    let boardPlane = new THREE.Mesh(boardPlaneGeometry, material);
+    boardPlane.rotation.x = Math.PI / 2;
+    boardPlane.position.set(halfSize, -0.01, halfSize);
+
+    scene.add(boardPlane);
+
+    // Create the grid.
+    let grid = new THREE.GridHelper(boardSize, boardSize);
+    grid.position.set(halfSize, 0, halfSize);
+
+    scene.add(grid);
+
+    camera.position.set(halfSize, boardSize, halfSize);
+    camera.lookAt(halfSize, 0, halfSize);
+});
 
 function animate() {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
 }
 animate();
+
+const board = [];
+for (let i = 0; i < boardSize; i++) {
+    board[i] = [];
+    for (let j = 0; j < boardSize; j++) {
+        board[i][j] = 0;
+    }
+}
