31f43f33bb2823d295083787262d153824848d77
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-08-29 09:21:57 -0500

    "Enhance game board functionality and interaction"
    Game board dimensions and grid have been enhanced. The board size has been updated to cater for the edges. Grid was converted into a 3-dimensional array of boxes, forming each cell in the grid to enable better interaction. An event listener was added to place stones, represented as 3D spheres, on the board upon a mouse click. These changes were made to better simulate an actual board game, adding interactive elements and features.

--- main.js
+++ main.js
@@ -8,41 +8,78 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// Loading and applying the texture
-loader.load('public/pandav-tank-88-KbSZbuEc-unsplash.jpg', function ( texture ) {
-    const material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.DoubleSide } );
+const boardSize = 15; // Including the edges
+let halfSize = boardSize / 2;
+let boardPlaneGeometry = new THREE.PlaneGeometry(boardSize, boardSize);
 
-    const boardSize = 10;
-    let halfSize = boardSize / 2;
-    let boardPlaneGeometry = new THREE.PlaneGeometry(boardSize, boardSize);
+let board = [];
+
+loader.load('pandav-tank-88-KbSZbuEc-unsplash.jpg', function ( woodTexture ) {
+    const boardMaterial = new THREE.MeshBasicMaterial( { map: woodTexture, side: THREE.DoubleSide } );
 
     // Add the game board as a simple plane first.
-    let boardPlane = new THREE.Mesh(boardPlaneGeometry, material);
+    let boardPlane = new THREE.Mesh(boardPlaneGeometry, boardMaterial);
     boardPlane.rotation.x = Math.PI / 2;
-    boardPlane.position.set(halfSize, -0.01, halfSize);
+    boardPlane.position.set(halfSize - 0.5, -0.01, halfSize - 0.5); // Center the board with grid
 
     scene.add(boardPlane);
 
     // Create the grid.
-    let grid = new THREE.GridHelper(boardSize, boardSize);
-    grid.position.set(halfSize, 0, halfSize);
+    for (let i = 0; i < boardSize; i++){
+        for (let j = 0; j < boardSize; j++){
+            // Define box properties
+            const geometry = new THREE.BoxGeometry(1, 0.01, 1);
+            const material = new THREE.MeshBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0 });
 
-    scene.add(grid);
+            // Construct box and place it
+            const box = new THREE.Mesh(geometry, material);
+            box.position.set(i, 0, j);
 
-    camera.position.set(halfSize, boardSize, halfSize);
-    camera.lookAt(halfSize, 0, halfSize);
+            // Add box to scene and to board array
+            scene.add(box);
+            board.push(box);
+        }
+    }
+
+    camera.position.set(halfSize - 0.5, boardSize, halfSize - 0.5);
+    camera.lookAt(halfSize - 0.5, 0, halfSize - 0.5);
 });
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+window.addEventListener('click', onClick, false);
+
+function onClick(event) {
+    // Normalized mouse coordinates
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    // Update the picking ray with the camera and mouse position
+    raycaster.setFromCamera(mouse, camera);
+
+    // Calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObjects(board, true);
+
+    // If an object is intersected, add a stone.
+    if (intersects.length > 0) {
+        // The first intersected object is the closest one
+        let pos = intersects[0].object.position;
+
+        // Create a geometry for the stone.
+        const stoneGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+        const stoneMaterial = new THREE.MeshBasicMaterial({color: 0x88cc88});
+        const stone = new THREE.Mesh(stoneGeometry, stoneMaterial);
+
+        // Setting the stone position to the clicked location
+        stone.position.set(pos.x, 0.5, pos.z);
+
+        scene.add(stone);
+    }
+}
 
 function animate() {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
 }
 animate();
-
-const board = [];
-for (let i = 0; i < boardSize; i++) {
-    board[i] = [];
-    for (let j = 0; j < boardSize; j++) {
-        board[i][j] = 0;
-    }
-}
