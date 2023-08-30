ae09cca978baa045a84f40bdbf57e1c56f7fd56c
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-08-30 17:22:29 -0500

    "Refactor scene rendering and interactions in main.js
    The changes were made to simplify the scene setup and interactions in main.js. This included removing the complex logic for creating the game board and replacing it with a simpler THREE.GridHelper. The texture loading for the game board was removed and lighting and controls were added for a better user experience. The interaction code was also refactored to allow interactions with the new grid layout. This refactoring goal is to make maintaining and expanding this code easier in the future."

--- main.js
+++ main.js
@@ -1,49 +1,31 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const loader = new THREE.TextureLoader();
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+const boardSize = 14; // Including the edges
 
-const boardSize = 15; // Including the edges
-let halfSize = boardSize / 2;
-let boardPlaneGeometry = new THREE.PlaneGeometry(boardSize, boardSize);
+const grid = new THREE.GridHelper(boardSize, boardSize);
+grid.position.set(0, 0, 0);
+grid.rotation.x = Math.PI / 2; // rotate 90 degrees
+scene.add(grid);
 
-let board = [];
+// Add ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
-loader.load('pandav-tank-88-KbSZbuEc-unsplash.jpg', function ( woodTexture ) {
-    const boardMaterial = new THREE.MeshBasicMaterial( { map: woodTexture, side: THREE.DoubleSide } );
+// Add directional light (like sunlight)
+const directLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directLight.position.set(0, 10, 10);
+scene.add(directLight);
 
-    // Add the game board as a simple plane first.
-    let boardPlane = new THREE.Mesh(boardPlaneGeometry, boardMaterial);
-    boardPlane.rotation.x = Math.PI / 2;
-    boardPlane.position.set(halfSize - 0.5, -0.01, halfSize - 0.5); // Center the board with grid
-
-    scene.add(boardPlane);
-
-    // Create the grid.
-    for (let i = 0; i < boardSize; i++){
-        for (let j = 0; j < boardSize; j++){
-            // Define box properties
-            const geometry = new THREE.BoxGeometry(1, 0.01, 1);
-            const material = new THREE.MeshBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0 });
-
-            // Construct box and place it
-            const box = new THREE.Mesh(geometry, material);
-            box.position.set(i, 0, j);
-
-            // Add box to scene and to board array
-            scene.add(box);
-            board.push(box);
-        }
-    }
-
-    camera.position.set(halfSize - 0.5, boardSize, halfSize - 0.5);
-    camera.lookAt(halfSize - 0.5, 0, halfSize - 0.5);
-});
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(0, 0, boardSize);
+camera.lookAt(0, 0, 0);
 
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
@@ -59,24 +41,32 @@ function onClick(event) {
     raycaster.setFromCamera(mouse, camera);
 
     // Calculate objects intersecting the picking ray
-    const intersects = raycaster.intersectObjects(board, true);
+    const intersects = raycaster.intersectObjects([grid], true);
 
     // If an object is intersected, add a stone.
     if (intersects.length > 0) {
         // The first intersected object is the closest one
-        let pos = intersects[0].object.position;
+        let pos = intersects[0].point;
 
         // Create a geometry for the stone.
-        const stoneGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-        const stoneMaterial = new THREE.MeshBasicMaterial({color: 0x88cc88});
+        const stoneGeometry = new THREE.CylinderGeometry(0.5, 0.5, 0.1, 32);
+        const stoneMaterial = new THREE.MeshPhongMaterial({
+            color: 0x000000, // black color
+            shininess: 100,   // make it shiny
+        });
         const stone = new THREE.Mesh(stoneGeometry, stoneMaterial);
 
         // Setting the stone position to the clicked location
-        stone.position.set(pos.x, 0.5, pos.z);
+        stone.position.set(pos.x, pos.y, pos.z + 0.05);
+        stone.rotation.x = Math.PI / 2;
 
         scene.add(stone);
     }
 }
+
+let axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+const controls = new OrbitControls( camera, renderer.domElement );
 
 function animate() {
     requestAnimationFrame( animate );
