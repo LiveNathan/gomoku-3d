3e58a31ef602ca6973c5f22ff3ecf9a801477927
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-09-01 10:11:04 -0500

    Add label styling and test utility, upgrade dependencies
    Modified style.css to add a class for labels, enhancing the UI by providing more specific styling options for labels. Installed the Jest testing library as a devDependency in package.json to expand testing capabilities and inform development. Upgraded 'three' dependency version for improved functionality and compatibility. Included 'three-css2drenderer' for better 2D rendering in the Three.js 3D library.

--- main.js
+++ main.js
@@ -1,33 +1,46 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import {CSS2DRenderer, CSS2DObject} from 'three/addons/renderers/CSS2DRenderer.js';
 
 const loader = new THREE.TextureLoader();
 
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
+const labelRenderer = new CSS2DRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+labelRenderer.setSize(window.innerWidth, window.innerHeight);
+labelRenderer.domElement.style.position = 'absolute';
+labelRenderer.domElement.style.top = '0px';
+document.body.appendChild(labelRenderer.domElement);
+
 const boardSize = 14; // Including the edges
+const halfBoardSize = boardSize / 2;  // 7
 const gameBoard = Array(boardSize).fill().map(() => Array(boardSize).fill(null)); // initialize the game board as 2D array
 
 const grid = new THREE.GridHelper(boardSize, boardSize);
-grid.position.set(0, 0, 0);
+grid.position.set(halfBoardSize, -halfBoardSize, 0);
 grid.rotation.x = Math.PI / 2; // rotate 90 degrees
+
+// Add grid labels
+for (let i = 0; i < boardSize + 1; i++) {
+    for (let j = 0; j < boardSize + 1; j++) {
+        const div = document.createElement('div');
+        div.className = 'label';
+        div.textContent = `(${i}, ${j})`;
+        // div.style.marginTop = '-1em';
+        const label = new CSS2DObject(div);
+        label.position.set(i, -j, 0);
+        scene.add(label);
+    }
+}
+
 scene.add(grid);
 
-// Add ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
-
-// Add directional light (like sunlight)
-const directLight = new THREE.DirectionalLight(0xffffff, 0.5);
-directLight.position.set(0, 10, 10);
-scene.add(directLight);
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, boardSize);
-camera.lookAt(0, 0, 0);
+camera.position.set(halfBoardSize, -halfBoardSize, boardSize);
+camera.lookAt(halfBoardSize, -halfBoardSize, 0);
 
 let currentPlayer = 'black';
 
@@ -36,13 +49,18 @@ const mouse = new THREE.Vector2();
 
 window.addEventListener('click', clickHandler, false);
 
-// Invisible plane for raycaster intersection
-const planeGeometry = new THREE.PlaneGeometry(boardSize, boardSize);
-const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide }); // red color for debugging
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.rotation.x = Math.PI / 2; // Rotate 90 degrees
-plane.position.z = 0.05; // Slightly above the grid for clickable intersections
-scene.add(plane);
+// Create a transparent plane to capture the mouse clicks
+const geometry = new THREE.PlaneGeometry(boardSize, boardSize);
+const material = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    side: THREE.DoubleSide,
+    transparent: true,
+    opacity: 0.2
+});
+const plane = new THREE.Mesh(geometry, material);
+plane.position.set(halfBoardSize, halfBoardSize, 0);
+
+// scene.add(plane);
 
 function clickHandler(event) {
     // Normalized mouse coordinates
@@ -54,7 +72,8 @@ function clickHandler(event) {
     raycaster.setFromCamera(mouse, camera);
 
     // Calculate objects intersecting the picking ray
-    const intersects = raycaster.intersectObjects(plane, true);
+    const intersects = raycaster.intersectObjects(scene.children);
+    console.log("Intersects: " + intersects.length);
 
     // If an object is intersected, add a stone.
     if (intersects.length > 0) {
@@ -65,19 +84,19 @@ function clickHandler(event) {
         console.log("Intersect position: " + intersectPositionX + ", " + intersectPositionY)
 
         // Create a geometry for the stone.
-        const stoneGeometry = new THREE.CylinderGeometry(0.5, 0.5, 0.1, 32);
-        const stoneMaterial = new THREE.MeshPhongMaterial({
-            color: currentPlayer === 'black' ? 0x000000 : 0xffffff, // set color based on current player
-            shininess: 100,   // make it shiny
-        });
-        const stone = new THREE.Mesh(stoneGeometry, stoneMaterial);
+        // const stoneGeometry = new THREE.CylinderGeometry(0.5, 0.5, 0.1, 32);
+        // const stoneMaterial = new THREE.MeshPhongMaterial({
+        //     color: currentPlayer === 'black' ? 0x000000 : 0xffffff, // set color based on current player
+        //     shininess: 100,   // make it shiny
+        // });
+        // const stone = new THREE.Mesh(stoneGeometry, stoneMaterial);
 
         // Setting the stone position to the clicked location
-        stone.position.set(intersectPosition.x, intersectPosition.y, intersectPosition.z + 0.05);
-        console.log(stone.position);
-        stone.rotation.x = Math.PI / 2;
+        // stone.position.set(intersectPosition.x, intersectPosition.y, intersectPosition.z + 0.05);
+        // console.log(stone.position);
+        // stone.rotation.x = Math.PI / 2;
 
-        scene.add(stone);
+        // scene.add(stone);
         //
         // let adjustedX = Math.round(intersectPosition.x) + (boardSize / 2);
         // console.log("adjusted x: " + adjustedX)
@@ -90,15 +109,18 @@ function clickHandler(event) {
         //     currentPlayer = currentPlayer === 'black' ? 'white' : 'black';
         // }
     }
+    return {intersectPositionX, intersectPositionY};
 }
 
 let axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
-const controls = new OrbitControls(camera, renderer.domElement);
+
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    labelRenderer.render(scene, camera);
 }
 
 animate();
